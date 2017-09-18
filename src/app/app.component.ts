364284948bdc11d327c22037e0e4129177b94cfc
Wibawa, Ryan (GE Digital, consultant) <ryan.wibawa@ge.com>
2017-09-17 21:40:03 -0700

    add component and template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: `<h1>{{title}}</h1>
+  //   <h2>{{hero.name}} details!</h2>
+  //   <div><label>id: </label>{{hero.id}}</div>
+  //   <div><label>name: </label>{{hero.name}}</div>`,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Navisow';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+
