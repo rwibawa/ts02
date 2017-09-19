e585b9ee036099bb5ed937850db5573d6cda9dab
Wibawa, Ryan (GE Digital, consultant) <ryan.wibawa@ge.com>
2017-09-19 15:33:05 -0700

    add Routing

--- README.md
+++ README.md
@@ -111,3 +111,12 @@ Before running the tests make sure you are serving the app via `ng serve`.
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## 3. Tutorial
+[Angular Tutorial](https://angular.io/tutorial)
+
+### build the app
+```bash
+$ ng build -prod
+$ ls -lah dist/
+```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule // <-- import it before binding with [(ngModule)]
+    FormsModule, // <-- import it before binding with [(ngModule)]
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
